fc4ad2018daddff91fd475a4f92b09b334136571
matthew <hillmj23@wfu.edu>
2025-01-28 20:36:02 -0500

    Forced Tesseract installation and debug info

--- render-build.sh
+++ render-build.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-apt-get update && apt-get install -y tesseract-ocr
+# Update package lists
+apt-get update 
+
+# Install Tesseract OCR and English language pack
+apt-get install -y tesseract-ocr tesseract-ocr-eng 
+
+# Print Tesseract version to confirm installation
+tesseract --version
+
+# Show where Tesseract is installed
+which tesseract
