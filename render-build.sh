de1be079eeed5c9fb117a01bb09ae43e3762556c
matthew <hillmj23@wfu.edu>
2025-02-10 14:21:26 -0500

    Added script to install Tesseract on Render

--- render-build.sh
+++ render-build.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-set -e  # Stop script on first error
 
-# Update package lists
-apt-get update 
-
-# Install Tesseract OCR and necessary language data
-apt-get install -y tesseract-ocr tesseract-ocr-eng
-
-# Show installation path of Tesseract
-which tesseract
-
-# Show Tesseract version to confirm installation
-tesseract --version
+# Update package list and install Tesseract OCR
+sudo apt-get update && sudo apt-get install -y tesseract-ocr
